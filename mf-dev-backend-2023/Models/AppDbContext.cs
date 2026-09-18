da5f06ea7c0fc5f2895a40e7d2a667c50f101539
Felipe <correaflp@hotmail.com.br>
2026-09-18 20:08:06 -0300

    db configuration initial

---
+++ mf-dev-backend-2023/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2023.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ mf-dev-backend-2023/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Vehicle")]
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obligated inform the name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Obligated inform the plate")]
+        public string Plate { get; set; }
+
+        [Required(ErrorMessage = "Obligated inform the year of manufacture")]
+        public int ManufactureYear { get; set; }
+
+        [Required(ErrorMessage = "Obligated inform the year of model")]
+        public int ModelYear { get; set; }
+    }
+}
